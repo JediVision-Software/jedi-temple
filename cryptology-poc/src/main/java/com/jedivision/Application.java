d53df7f12af6d8abaf49a253ea21b75607fda2a5
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2017-05-17 13:51:49 +0300

    add cryptology-poc apache maven skeleton + LOGGER

---
+++ cryptology-poc/src/main/java/com/jedivision/Application.java
@@ -0,0 +1,13 @@
+package com.jedivision;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Application {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Cryptology PoC started...");
+        LOGGER.info("Cryptology PoC finished...");
+    }
+}
