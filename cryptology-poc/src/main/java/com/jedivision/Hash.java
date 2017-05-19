b2829d38a110512474e11201d9d5e1d74ebe562f
Yurii Luchkiv <yurii.luchkiv@gmail.com>
2017-05-19 15:31:58 +0300

    add hash: md5 via commons codec

--- cryptology-poc/src/main/java/com/jedivision/Hash.java
+++ cryptology-poc/src/main/java/com/jedivision/Hash.java
@@ -1,5 +1,6 @@
 package com.jedivision;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,8 +10,8 @@ import java.security.NoSuchAlgorithmException;
 public class Hash {
     private static final Logger LOGGER = LoggerFactory.getLogger(Hash.class);
 
-    public static String md5(String value) throws NoSuchAlgorithmException {
-        LOGGER.debug("Encoding {} with md5 hashing algorithm {}", value);
+    public static String md5ViaPureJava(String value) throws NoSuchAlgorithmException {
+        LOGGER.debug("Encoding {} with md5 hashing algorithm via pure Java", value);
         MessageDigest md = MessageDigest.getInstance("MD5");
         md.update(value.getBytes());
         byte[] digest = md.digest();
@@ -19,5 +20,10 @@ public class Hash {
             sb.append(String.format("%02x", b & 0xff));
         }
         return sb.toString();
+    }
+
+    public static String md5ViaCommonsCodec(String value) {
+        LOGGER.debug("Encoding {} with md5 hashing algorithm via commons codec", value);
+        return DigestUtils.md5Hex(value);
     }
 }
--- cryptology-poc/src/test/java/com/jedivision/HashTest.java
+++ cryptology-poc/src/test/java/com/jedivision/HashTest.java
@@ -4,19 +4,30 @@ import org.junit.Test;
 
 import java.security.NoSuchAlgorithmException;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
 
 public class HashTest {
 
-    @Test
-    public void md5() throws NoSuchAlgorithmException {
-        // Arrange
-        String value = "JDV";
+    private static final String md5Value = "JDV";
+    private static final String md5EncodedValue = "d7df1f0388aa696ce6ca0750afdebda5";
 
+    @Test
+    public void md5ViaPureJava() throws NoSuchAlgorithmException {
         // Act
-        String encodedValue = Hash.md5(value);
+        String encodedValue = Hash.md5ViaPureJava(md5Value);
 
         // Assert
-        assertEquals("d7df1f0388aa696ce6ca0750afdebda5", encodedValue);
+        assertThat(encodedValue, equalTo(encodedValue));
     }
+
+    @Test
+    public void md5ViaCommonsCodec() throws NoSuchAlgorithmException {
+        // Act
+        String encodedValue = Hash.md5ViaCommonsCodec(md5Value);
+
+        // Assert
+        assertThat(encodedValue, equalTo(encodedValue));
+    }
+
 }
