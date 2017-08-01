c8354c2ff62c0c4013228927144fd07ff3847a85
Pavlo Pastushok <pavlo.pastushok@gmail.com>
2017-08-01 23:18:46 +0300

    add RC2 encryption/decryption

--- cryptology-poc/src/main/java/com/jedivision/Encryption.java
+++ cryptology-poc/src/main/java/com/jedivision/Encryption.java
@@ -2,6 +2,7 @@ package com.jedivision;
 
 import javax.crypto.*;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.RC2ParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
 import java.security.*;
@@ -10,12 +11,14 @@ import java.util.Base64;
 public class Encryption {
     // Constants
     private static final String AES = "AES";
+    private static final String RC2 = "RC2";
     private static final String BLOWFISH = "Blowfish";
     private static final String RSA = "RSA";
     private static final String UTF8 = "UTF-8";
     // Cipher instances constants (Algorithm name/Mode (optional)/Padding scheme (optional))
     private static final String CIPHER_AES_INSTANCE = "AES/CBC/PKCS5PADDING";
     private static final String CIPHER_DES_INSTANCE = "DES/ECB/PKCS5PADDING";
+    private static final String CIPHER_RC2_INSTANCE = "RC2/CBC/PKCS5PADDING";
 
     public static String encryptAES(String key, String initVector, String value) throws UnsupportedEncodingException,
                                                                                         NoSuchPaddingException,
@@ -28,7 +31,7 @@ public class Encryption {
         SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(UTF8), AES);
         Cipher cipher = Cipher.getInstance(CIPHER_AES_INSTANCE);
         cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, iv);
-        byte[] encrypted = cipher.doFinal(value.getBytes());
+        byte[] encrypted = cipher.doFinal(value.getBytes(UTF8));
         return Base64.getEncoder().encodeToString(encrypted);
     }
 
@@ -74,10 +77,10 @@ public class Encryption {
     }
 
     public static String encryptDES(SecretKey desKey, String value) throws NoSuchPaddingException,
-                                                                            NoSuchAlgorithmException,
-                                                                            InvalidKeyException,
-                                                                            BadPaddingException,
-                                                                            IllegalBlockSizeException {
+            NoSuchAlgorithmException,
+            InvalidKeyException,
+            BadPaddingException,
+            IllegalBlockSizeException {
         Cipher cipher = Cipher.getInstance(CIPHER_DES_INSTANCE);
         cipher.init(Cipher.ENCRYPT_MODE, desKey);
         byte[] encrypted = cipher.doFinal(value.getBytes());
@@ -85,12 +88,40 @@ public class Encryption {
     }
 
     public static String decryptDES(SecretKey desKey, String encrypted) throws NoSuchPaddingException,
-                                                                            NoSuchAlgorithmException,
-                                                                            InvalidKeyException,
-                                                                            BadPaddingException,
-                                                                            IllegalBlockSizeException {
+            NoSuchAlgorithmException,
+            InvalidKeyException,
+            BadPaddingException,
+            IllegalBlockSizeException {
         Cipher cipher = Cipher.getInstance(CIPHER_DES_INSTANCE);
         cipher.init(Cipher.DECRYPT_MODE, desKey);
+        byte[] original = cipher.doFinal(Base64.getDecoder().decode(encrypted));
+        return new String(original);
+    }
+
+    public static String encryptRC2(String key, String initVector, String value) throws NoSuchPaddingException,
+                                                                                        NoSuchAlgorithmException,
+                                                                                        InvalidAlgorithmParameterException,
+                                                                                        InvalidKeyException,
+                                                                                        BadPaddingException,
+                                                                                        IllegalBlockSizeException,
+                                                                                        UnsupportedEncodingException {
+        RC2ParameterSpec ivSpec = new RC2ParameterSpec(key.getBytes(UTF8).length, initVector.getBytes(UTF8));
+        Cipher cipher = Cipher.getInstance(CIPHER_RC2_INSTANCE);
+        cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(key.getBytes(), RC2), ivSpec);
+        byte[] encrypted = cipher.doFinal(value.getBytes());
+        return Base64.getEncoder().encodeToString(encrypted);
+    }
+
+    public static String decryptRC2(String key, String initVector, String encrypted) throws NoSuchPaddingException,
+                                                                                            NoSuchAlgorithmException,
+                                                                                            InvalidAlgorithmParameterException,
+                                                                                            InvalidKeyException,
+                                                                                            BadPaddingException,
+                                                                                            IllegalBlockSizeException,
+                                                                                            UnsupportedEncodingException {
+        RC2ParameterSpec ivSpec = new RC2ParameterSpec(key.getBytes(UTF8).length, initVector.getBytes(UTF8));
+        Cipher cipher = Cipher.getInstance(CIPHER_RC2_INSTANCE);
+        cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(key.getBytes(), RC2), ivSpec);
         byte[] original = cipher.doFinal(Base64.getDecoder().decode(encrypted));
         return new String(original);
     }
--- cryptology-poc/src/test/java/com/jedivision/EncryptionTest.java
+++ cryptology-poc/src/test/java/com/jedivision/EncryptionTest.java
@@ -13,9 +13,10 @@ import static org.junit.Assert.assertThat;
 public class EncryptionTest {
     private static final String GLOBAL_VALUE = "JDV";
     private static final String GLOBAL_ENCRYPTED_AES_VALUE = "qApcic/FO5kfjyhYIEKs7w==";
+    private static final String GLOBAL_ENCRYPTED_RC2_VALUE = "ww6eJe7S7Xw=";
     private static final String GLOBAL_ENCRYPTED_BLOWFISH_VALUE = "quFqbk4Yuxk=";
-    private static final String AES_KEY = "Zq4t7w!z%C*F-JdV";
-    private static final String AES_INIT_VECTOR = "Random_JDVVector";
+    private static final String ENCRYPTION_KEY = "Zq4t7w!z%C*F-JdV";
+    private static final String ENCRYPTION_INIT_VECTOR = "Random_JDVVector";
     private static final String BLOWFISH_USERNAME = "username";
     private static final String BLOWFISH_PASSWORD = "password";
     private static final String RSA = "RSA";
@@ -52,7 +53,7 @@ public class EncryptionTest {
                                         NoSuchAlgorithmException,
                                         InvalidAlgorithmParameterException {
         // Act
-        String encodedValue = Encryption.encryptAES(AES_KEY, AES_INIT_VECTOR, GLOBAL_VALUE);
+        String encodedValue = Encryption.encryptAES(ENCRYPTION_KEY, ENCRYPTION_INIT_VECTOR, GLOBAL_VALUE);
 
         // Assert
         assertThat(encodedValue, equalTo(GLOBAL_ENCRYPTED_AES_VALUE));
@@ -67,7 +68,7 @@ public class EncryptionTest {
                                         NoSuchAlgorithmException,
                                         InvalidAlgorithmParameterException {
         // Act
-        String decodedValue = Encryption.decryptAES(AES_KEY, AES_INIT_VECTOR, GLOBAL_ENCRYPTED_AES_VALUE);
+        String decodedValue = Encryption.decryptAES(ENCRYPTION_KEY, ENCRYPTION_INIT_VECTOR, GLOBAL_ENCRYPTED_AES_VALUE);
 
         // Assert
         assertThat(decodedValue, equalTo(GLOBAL_VALUE));
@@ -120,6 +121,36 @@ public class EncryptionTest {
                                         NoSuchPaddingException {
         // Act
         String encodedValue = Encryption.decryptDES(desKey, globalEncryptedDesValue);
+
+        // Assert
+        assertThat(encodedValue, equalTo(GLOBAL_VALUE));
+    }
+
+    @Test
+    public void encryptRC2Test() throws NoSuchPaddingException,
+                                        InvalidKeyException,
+                                        NoSuchAlgorithmException,
+                                        IllegalBlockSizeException,
+                                        BadPaddingException,
+                                        InvalidAlgorithmParameterException,
+                                        UnsupportedEncodingException {
+        // Act
+        String encodedValue = Encryption.encryptRC2(ENCRYPTION_KEY, ENCRYPTION_INIT_VECTOR, GLOBAL_VALUE);
+
+        // Assert
+        assertThat(encodedValue, equalTo(GLOBAL_ENCRYPTED_RC2_VALUE));
+    }
+
+    @Test
+    public void decryptRC2Test() throws NoSuchPaddingException,
+                                        InvalidKeyException,
+                                        NoSuchAlgorithmException,
+                                        IllegalBlockSizeException,
+                                        BadPaddingException,
+                                        InvalidAlgorithmParameterException,
+                                        UnsupportedEncodingException {
+        // Act
+        String encodedValue = Encryption.decryptRC2(ENCRYPTION_KEY, ENCRYPTION_INIT_VECTOR, GLOBAL_ENCRYPTED_RC2_VALUE);
 
         // Assert
         assertThat(encodedValue, equalTo(GLOBAL_VALUE));
