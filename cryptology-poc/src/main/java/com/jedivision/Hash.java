69ade2c2407c9ad8768a371e5523b971e94bc295
Pavlo Pastushok <pavlo.pastushok@gmail.com>
2017-08-16 21:33:25 +0300

    add sha256ViaPureJava hashing and test

--- cryptology-poc/src/main/java/com/jedivision/Hash.java
+++ cryptology-poc/src/main/java/com/jedivision/Hash.java
@@ -15,6 +15,7 @@ public class Hash {
     private static final Logger LOGGER = LoggerFactory.getLogger(Hash.class);
     private static final String MD5 = "MD5";
     private static final String SHA1 = "SHA-1";
+    private static final String SHA256 = "SHA-256";
 
     public static String md5ViaPureJava(String value) throws NoSuchAlgorithmException {
         LOGGER.debug("Hashing {} with md5 hashing algorithm via pure Java", value);
@@ -48,6 +49,18 @@ public class Hash {
     public static String sha1ViaCommonsCodec(String value) {
         LOGGER.debug("Hashing {} with sha-1 hashing algorithm via commons codec", value);
         return DigestUtils.sha1Hex(value);
+    }
+
+    public static String sha256ViaPureJava(String value) throws NoSuchAlgorithmException {
+        LOGGER.debug("Hashing {} with sha1 hashing algorithm via pure Java", value);
+        MessageDigest md = MessageDigest.getInstance(SHA256);
+        md.update(value.getBytes(UTF_8));
+        byte[] digest = md.digest();
+        StringBuilder sb = new StringBuilder();
+        for (byte b : digest) {
+            sb.append(String.format("%02x", b & 0xff));
+        }
+        return sb.toString();
     }
 
     public static String sha256ViaCommonsCodec(String value) {
--- cryptology-poc/src/test/java/com/jedivision/HashTest.java
+++ cryptology-poc/src/test/java/com/jedivision/HashTest.java
@@ -14,7 +14,7 @@ public class HashTest {
 
     private static final String GLOBAL_VALUE = "JDV";
     private static final String GLOBAL_ENCODED_MD5_VALUE = "d7df1f0388aa696ce6ca0750afdebda5";
-    private static final String GLOBAL_ENCODED_SHA_1_VALUE = "a624672ff7e7160788261f0cec3136a7s89ac758a";
+    private static final String GLOBAL_ENCODED_SHA_1_VALUE = "a624672ff7e7160788261f0cec3136a789ac758a";
     private static final String GLOBAL_ENCODED_SHA_256_VALUE = "d5a5d16c2b9261544191204c5e3344071b0cbc064cbd04a8b9238b946bd7a5ad";
     private static final String GLOBAL_ENCODED_SHA_384_VALUE = "1ae05faecad3bf1261fb0896b7ac3ef7460c776102ecb03432f83b680570f21ac5f4836e8a234d489f456af7ef0b468d";
     private static final String GLOBAL_ENCODED_SHA_512_VALUE = "5c840b09e9f7d93991bc59c7a6aad6457f294cfb0edc2522e5117393c51fdf8dc6793d77dda284ef000d3c8de2561b31199c64c3231c7e4a7bb0c63f795422fd";
@@ -38,6 +38,15 @@ public class HashTest {
     }
 
     @Test
+    public void sha1ViaPureJavaTest() throws NoSuchAlgorithmException {
+        // Act
+        String encodedValue = Hash.sha1ViaPureJava(GLOBAL_VALUE);
+
+        // Assert
+        assertThat(encodedValue, equalTo(GLOBAL_ENCODED_SHA_1_VALUE));
+    }
+
+    @Test
     public void sha1ViaCommonsCodecTest() throws NoSuchAlgorithmException {
         // Act
         String encodedValue = Hash.sha1ViaCommonsCodec(GLOBAL_VALUE);
@@ -47,12 +56,12 @@ public class HashTest {
     }
 
     @Test
-    public void sha1ViaPureJavaTest() throws NoSuchAlgorithmException {
+    public void sha256ViaPureJavaTest() throws NoSuchAlgorithmException {
         // Act
-        String encodedValue = Hash.sha1ViaPureJava(GLOBAL_VALUE);
+        String encodedValue = Hash.sha256ViaPureJava(GLOBAL_VALUE);
 
         // Assert
-        assertThat(encodedValue, equalTo(GLOBAL_ENCODED_SHA_1_VALUE));
+        assertThat(encodedValue, equalTo(GLOBAL_ENCODED_SHA_256_VALUE));
     }
 
     @Test
