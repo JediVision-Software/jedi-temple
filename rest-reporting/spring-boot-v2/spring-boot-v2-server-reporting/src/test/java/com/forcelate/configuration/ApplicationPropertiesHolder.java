4c64ab6492b745fb992c0e6b17e2119a592a529d
Oleg Gema <oleggema@gmail.com>
2019-03-11 16:51:13 +0200

    ports updated

--- rest-reporting/spring-boot-v2/spring-boot-v2-server-reporting/src/test/java/com/forcelate/configuration/ApplicationPropertiesHolder.java
+++ rest-reporting/spring-boot-v2/spring-boot-v2-server-reporting/src/test/java/com/forcelate/configuration/ApplicationPropertiesHolder.java
@@ -1,44 +1,16 @@
 package com.forcelate.configuration;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.web.server.i18n.AcceptHeaderLocaleContextResolver;
-import org.springframework.web.server.i18n.LocaleContextResolver;
 
-import java.util.Locale;
-
-@Configuration
 @ComponentScan("com")
-@PropertySource("classpath:application-dev.properties")
 @Slf4j
 public class ApplicationPropertiesHolder {
-
     @Bean
     public static PropertySourcesPlaceholderConfigurer placeholder() {
         return new PropertySourcesPlaceholderConfigurer();
     }
-
-    @Bean
-    public MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("locale/messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        messageSource.setAlwaysUseMessageFormat(true);
-        return messageSource;
-    }
-
-    @Bean
-    public LocaleContextResolver localeResolver() {
-        AcceptHeaderLocaleContextResolver sessionLocaleResolver = new AcceptHeaderLocaleContextResolver();
-        sessionLocaleResolver.setDefaultLocale(Locale.ENGLISH);
-        return sessionLocaleResolver;
-    }
-
 }
 
